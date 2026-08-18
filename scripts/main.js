ac54571227251eefd238c47d7f1234bf63cf873e
vlad bronnikov <bronnikovvd@gmail.com>
2026-08-18 03:00:40 +0300

    Wrote styles for todo

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ import Header from './Header.js'
 import FormsValidation from './Form.js'
 import CustomFont from './Font.js'
 import Preloader from './Preloader.js'
+import Todo from './Todo.js'
 
 const rootProject = document.querySelector('[data-js-root]')
 
@@ -9,3 +10,4 @@ new CustomFont()
 new Header(rootProject)
 new FormsValidation()
 new Preloader(rootProject)
+new Todo()
